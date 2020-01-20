70ffcaf9f380fbdcdd2368af084b3a9e64494001
Yuichi Tanaka <yuichielectric@github.com>
2020-01-21 00:09:42 +0900

    Fix invalid ssh key error

--- ghes/main.tf
+++ ghes/main.tf
@@ -19,7 +19,7 @@ variable "ghes-version" {
 
 variable "ssh_public_key" {
   type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+wAPz1BqEtghow5fc+GoTb6dgXj2NIuQf0nGd6tKM5 yuichielectric@github.com"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdpWKK9P//6nT+hI5s3d4y9fMM/5AYP9yl1i/3qWX/CsA1DXvA2+sqXL2j3s0ngx3vgXE+nNRXvCGTPlw1jotprqlsLijjhBH04uwzIfpFgp1ZY1xRaX1x3EfbyS7uEtsvhZ06ckbZREFk50SEKcMov7EIIWV5QH//143OwRna8RfKL5HAEF5prexTOZii0OC6a8QGZ9w1wDA3kZJdZGxp0W/cV+EhUNG9w+rdLSiO5op9vcr+65DKO0k+a03JD3S7mFBXe19POSVTKCWXzkxsB1SgT6Re/pJyBu7KKMTPtz6brstJlgGgiaNdJ9WDMuK+mFO074GBnl/2w99hqzGd yuichielectric@github.com"
 }
 
 terraform {
